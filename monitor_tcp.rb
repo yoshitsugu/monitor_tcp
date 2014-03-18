d252111c91f5e5044b2e8b17f9cff3b90e37129b
吉次 孝太 <k.yoshitsugu@mmj.ne.jp>
2014-03-18 17:17:54 +0900

    普通のtimeoutだとRubyレベルの処理でtimeoutは可能だが、Cライブラリ内でブロックされるとtimeoutが検知できない

--- monitor_tcp.rb
+++ monitor_tcp.rb
@@ -1,6 +1,7 @@
 require 'socket'
 require 'mail'
 require 'logger'
+require 'timeout'
 require File.expand_path("config.rb", File.dirname(__FILE__))
 
 class TcpTester
@@ -11,14 +12,36 @@ class TcpTester
   end
 
   def main
-    timeout(@timeout) do
+    fork_timeout(@timeout) do 
       TCPSocket.open(@host, @port)
     end
   end
+
+  class ForkTimeoutError < RuntimeError; end
+
+  # http://docs.ruby-lang.org/ja/2.1.0/class/Timeout.html
+  # 普通のtimeoutだとRubyレベルの処理でtimeoutは可能だが、Cライブラリ内でブロックされるとtimeoutが検知できない。
+  def fork_timeout(secs, step=0.1, &block)
+    pid = Process.fork(&block)
+    unless _wait_until(pid, secs, step) then
+      Process.kill(:KILL, pid)
+      raise ForkTimeoutError
+    end
+  end
+  
+  def _wait_until(pid, secs, step)
+    (0.0).step(secs, step) do
+      sleep(0.1)
+      fin = Process.waitpid(pid, Process::WNOHANG)
+      return fin unless fin.nil?
+    end
+    nil
+  end
 end
 
-logger = Logger.new(File.expand_path("log.log", File.dirname(__FILE__)))
+
 begin
+  logger = Logger.new(File.expand_path("log.log", File.dirname(__FILE__)))
   if CONFIG.nil?
     raise "CONFIGを設定してね" 
   end
